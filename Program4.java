bb9a2ee2b8a54f16fb50f603d682b8dc251f047d
aleu7819 <allison.asl.leung@gmail.com>
2024-08-13 15:17:04 -0700

    Update Program4.java
    added in variables, calc, and the console output

--- Program4.java
+++ Program4.java
@@ -1,10 +1,29 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
 
+public class program4{
+    public static void main(String[] args){
+//assign number to variables
 
+int number1 = 475;
+int number2 = 821;
+int number3 = 369;
+int number4 = 562;
 
+// store sum in 5th variable
+int sum = number1+number2+number3+number4;
+
+//print average in 6th variable
+double average = (number1+number2+number3+number4)/4.0;
+//print results
+System.out.println("The sum of the four numbers is: " + sum);
+System.out.println("the average of the four numbers is: " + average);
+}
+}
 //Paste console output below:
 /*
 
+ The sum of the four numbers is: 2227
+the average of the four numbers is: 556.75
 
 */
